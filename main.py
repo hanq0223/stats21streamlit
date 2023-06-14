9d90d290d098562d623fa64d7498247f63316f28
Han Qiang <hanq0223@163.com>
2023-06-14 15:02:02 -0700

    new main

--- main.py
+++ main.py
@@ -1,51 +1,81 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+import io
+
+web_apps = st.sidebar.selectbox("Select Web Apps",
+                                ("Exploratory Data Analysis", "Distributions"))
 
 
-uploaded_file = st.file_uploader("Choose a file")
+if web_apps == "Exploratory Data Analysis":
 
-data = pd.read_csv(uploaded_file)
-shape = data.shape
-row_count = shape[0]
-col_count = shape[1]
+  uploaded_file = st.sidebar.file_uploader("Choose a file")
 
-st.write("number of rows:", row_count)
-st.write("number of columns:", col_count)
+  if uploaded_file is not None:
+    # Can be used wherever a "file-like" object is accepted:
+    df = pd.read_csv(uploaded_file)
+    show_df = st.checkbox("Show Data Frame", key="disabled")
 
-numeric_count = 0
-for column in data.columns:
-    if pd.api.types.is_numeric_dtype(data[column]):
-        numeric_count += 1
+    if show_df:
+      st.write(df)
 
+    column_type = st.sidebar.selectbox('Select Data Type',
+                                       ("Numerical", "Categorical", "Bool", "Date"))
 
-categorical_count = 0
-for column in data.columns:
-    if pd.api.types.is_string_dtype(data[column]):
-       categorical_count += 1
+    if column_type == "Numerical":
+      numerical_column = st.sidebar.selectbox(
+          'Select a Column', df.select_dtypes(include=['int64', 'float64']).columns)
 
+      # histogram
+      choose_color = st.color_picker('Pick a Color', "#69b3a2")
+      choose_opacity = st.slider(
+          'Color Opacity', min_value=0.0, max_value=1.0, step=0.05)
 
-st.write("number of numeric variable:", numeric_count)
-st.write("number of categorical variable:", categorical_count)
+      hist_bins = st.slider('Number of bins', min_value=5,
+                            max_value=150, value=30)
+      hist_title = st.text_input('Set Title', 'Histogram')
+      hist_xtitle = st.text_input('Set x-axis Title', numerical_column)
 
-selected_col = st.selectbox( "Select one of the columns", data.columns)
-data_col = data[selected_col]
+      fig, ax = plt.subplots()
+      ax.hist(df[numerical_column], bins=hist_bins,
+              edgecolor="black", color=choose_color, alpha=choose_opacity)
+      ax.set_title(hist_title)
+      ax.set_xlabel(hist_xtitle)
+      ax.set_ylabel('Count')
 
-st.write(selected_col, ":")
-if pd.api.types.is_numeric_dtype(data_col):
-    st.table(data_col.describe())
-    fig, ax = plt.subplots()
-    ax.hist(data_col, bins=20, color = 'red')
-    st.pyplot(fig)
-else:
-    prop = data_col.value_counts(normalize=True)
-    st.write(prop)
-    fig2, ax2 = plt.subplots()
-    prop.plot(kind='bar', ax = ax2 ,color = 'purple')
-    st.pyplot(fig2)
-    
+      st.pyplot(fig)
+      filename = "plot.png"
+      fig.savefig(filename,dpi = 300)
 
+      # Display the download button
+      with open("plot.png", "rb") as file:
+        btn = st.download_button(
+            label="Download image",
+            data=file,
+            file_name="flower.png",
+            mime="image/png"
 
+       )  
+    if column_type == "Categorical":
+      categorical_column = st.sidebar.selectbox(
+          'Select a Column', df.select_dtypes(exclude=['int64', 'float64']).columns)
 
+      # histogram
+      choose_color = st.color_picker('Pick a Color', "#69b3a2")
+      choose_opacity = st.slider(
+          'Color Opacity', min_value=0.0, max_value=1.0, step=0.05)
+      hist_bins = st.slider('Number of bins', min_value=5,
+                            max_value=150, value=30)
+      hist_title = st.text_input('Set Title', 'Histogram')
+      hist_xtitle = st.text_input('Set x-axis Title', categorical_column)
 
+      fig, ax = plt.subplots()
+      ax.hist(df[categorical_column], bins=hist_bins,
+              edgecolor="black", color=choose_color, alpha=choose_opacity)
+      ax.set_title(hist_title)
+      ax.set_xlabel(hist_xtitle)
+      ax.set_ylabel('Count')
+
+      st.pyplot(fig)
+      filename = "plot.png"
+      fig.savefig(filename,dpi = 300)
